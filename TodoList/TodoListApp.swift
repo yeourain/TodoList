c0bc92845c14ad2f503b3714801de51e135b6e3f
yeourain <yeourain@gmail.com>
2022-04-08 14:02:58 +1000

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by HYUN LEE on 8/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
